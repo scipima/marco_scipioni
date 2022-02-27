886438d82c58e2fb5ccd2603b9245202b6b4cd66
scipima <marco.scipioni05@gmail.com>
2022-02-27 01:30:02 +0100

    included script. changes to .Rproj

---
+++ apreshill_tutorial.R
@@ -0,0 +1,5 @@
+# apres hill tutorial
+install.packages("distill")
+library(distill)
+create_website(dir = ".", title = "iyo-distill", gh_pages = TRUE)
+# Please close the RStudio IDE and re-open it. 
