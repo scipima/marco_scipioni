6bdcf54b614a8cb6ab02b0f259b15ac03c7cb204
scipima <marco.scipioni05@gmail.com>
2022-02-27 01:41:43 +0100

    updating website structure

--- apreshill_tutorial.R
+++ apreshill_tutorial.R
@@ -3,3 +3,4 @@ install.packages("distill")
 library(distill)
 create_website(dir = ".", title = "iyo-distill", gh_pages = TRUE)
 # Please close the RStudio IDE and re-open it. 
+postcards::create_postcard(template = "jolla", file = "about.Rmd") 
